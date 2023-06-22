4f161dc1cbc8292270af1b4e4ce93378aad83061
SucreReina <chisommary442@yahoo.com>
2023-06-21 20:47:10 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-void print_fibonacci_sequence() {
-    int count = 98;
-    int previous = 1;
-    int current = 2;
-    int i;
+int main(void) print_fibonacci_sequence()
+{
+int count = 98;
+int previous = 1;
+int current = 2;
+int i;
 
-    printf("%d, %d", previous, current);
-
-    for (i = 3; i <= count; i++) {
-        int next = previous + current;
-        printf(", %d", next);
-        previous = current;
-        current = next;
-    }
-
-    printf("\n");
+printf("%d, %d", previous, current);
+for (i = 3; i <= count; i++)
+{                 
+int next = previous + current;
+printf(", %d", next);
+previous = current;
+current = next;
+}
+printf("\n");
 }
 
-int main() {
-    print_fibonacci_sequence();
-    return 0;
+int main(void)
+{
+print_fibonacci_sequence();
+return (0);
 }
 
 
