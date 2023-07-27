c185b2a449463d9a0056878511f3a2518293ef7b
SucreReina <chisommary442@yahoo.com>
2023-07-27 20:13:05 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  *    * @str: new string to add in the node
  *     *
  *      * Return: the address of the new element, or NULL if it fails
- *       */
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
