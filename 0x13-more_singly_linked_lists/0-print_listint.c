24c35449a1435812c9e6db11ab5ea1bb46776c55
SucreReina <chisommary442@yahoo.com>
2023-07-31 17:06:26 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 0;
-	const listint_t *current_node = h;
 
-
-	Traverse the list and print elements
-		while (current_node != NULL)
-		{
-			printf("%d\n", current_node->n);
-			current_node = current_node->next;
-			node_count++;
-		}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		node_count++;
+		h = h->next;
+	}
 
 	return (node_count);
 }
