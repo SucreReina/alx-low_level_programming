d804a76877340a8b26b3c490b6b75cc65496d3e4
SucreReina <chisommary442@yahoo.com>
2023-08-24 20:50:10 +0100

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,9 +4,11 @@
  *  * flip_bits - counts the number of bits to change
  *   * to get from one number to another
  *    * @n: first number
- *     * @m: second number      
+ *     * @m: second number
+ *      *
  *       * Return: number of bits to change
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
